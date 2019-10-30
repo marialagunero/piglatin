fe5bddcf939c061e86b54257987154e3b38e4a82
Student <example@example.com>
2019-10-30 14:32:21 -0700

    add var and array. still working on functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-/* if character is non-alphabetical, the program does nothing
+/* if character is non-alphabetical, the program does nothing XXX
 else if add "ay" to single-letter words beginning with a vowel
 else if word begins vowel, add "way" to end
 else if word begins "y", "y" = consonant
 else if word begins 1 or more consonants, move all first consonants to end and add "ay"
-else if first consonants are "qu" move both letters to end */
+else if first consonants are "qu" move both letters to end
+else */
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var english =
+    var pigLatin =
+    var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"];
+    var qu = ["Qu", "qu"]
+
+    var sentence = $("input#words").val();
+     for
+  });
+});
